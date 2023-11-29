ff7fce1a6e858be9252fa054839a718241062f87
Dinesh-SF4291 <149996130+Dinesh-SF4291@users.noreply.github.com>
2023-11-29 14:25:34 +0530

    Add project files.

---
+++ SyncServer/Data/Server.cs
@@ -0,0 +1,27 @@
+﻿namespace SyncServer.Data
+{
+    public class Server
+    {
+        public string ServerName { get; set; }
+        public string PortNumber { get; set; }
+        public string UserID { get; set; }
+        public string Password { get; set; }
+
+        public Server(string serverName,string portNumber,string userID,string password)
+        {
+            ServerName = serverName;
+            PortNumber = portNumber;
+            UserID = userID;
+            Password = password;
+        }
+        public Server() { } 
+        public Server(string connection)
+        {
+            string[] connectionArray=connection.Split(",");
+            ServerName = connectionArray[0];    
+            PortNumber = connectionArray[1];
+            UserID = connectionArray[2];
+            Password = connectionArray[3];
+        }
+    }
+}
